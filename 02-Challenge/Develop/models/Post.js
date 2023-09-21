956587631ffbb1946b30d7d5a244544d4f27316d
lwalker <landen@example.com>
2023-09-21 09:56:42 -0500

    models for comments and posts created and index.js file updated

--- 02-Challenge/Develop/models/Post.js
+++ 02-Challenge/Develop/models/Post.js
@@ -2,19 +2,23 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Comment extends Model {
+class Post extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-Comment.init(
+Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     content: {
       type: DataTypes.STRING,
@@ -29,12 +33,18 @@ Comment.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'comment',
+    modelName: 'post',
   }
 );
 
-module.exports = Comment;
+module.exports = Post;
--- 02-Challenge/Develop/models/index.js
+++ 02-Challenge/Develop/models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-module.exports = { User };
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+module.exports = { User, Post, Comment };
